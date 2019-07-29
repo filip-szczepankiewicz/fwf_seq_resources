345436aec2d3997d01bf390e99eb80ed606dcab3
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2019-07-28 20:14:07 -0400

    Added brain mk2 three level schemes

---
+++ SamplingSchemes/Brain_mk2/dvs_brain_mk2.m
@@ -0,0 +1,31 @@
+
+
+%% Short sampling scheme
+clear
+listname = 'brain_mk2_short';
+
+s = {'LTE', 'STE'};
+b = {[.1 1 2], [.1 1 2]};
+n = {[ 4 -10 -15 ], [ 6 10 10]};
+
+dvs_create(listname, b, n, s)
+
+%% Intermediate sampling scheme
+clear
+listname = 'brain_mk2_interm';
+
+s = {'LTE', 'STE'};
+b = {[ .1  .7  1.4  2  2], [ .1  .7  1.4  2]};
+n = {[ -6  -6 -10 -15  -6], [ -6  -6 -10 -15 ]};
+
+dvs_create(listname, b, n, s)
+
+%% Long sampling scheme
+clear
+listname = 'brain_mk2_long';
+
+s = {'LTE', 'PTE', 'STE'};
+b = {[.1 .7  1.4 1.4  2], [.1   1   2], [ .1 .7  1.4  2]};
+n = {[ -6 -6 -10 -6 -30.1], [ -6  -10  -15], [  6  6  10   10]};
+
+dvs_create(listname, b, n, s)
